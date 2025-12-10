304e6a1bf3f49e8c42dc47a08d59bda083295bb3
Shraddha Itaj <shraddhaitaj@gmail.com>
2025-12-10 20:06:07 +0530

    rect area peri

---
+++ geomatric.py
@@ -0,0 +1,5 @@
+def rectangle_area(len,br):
+    print("area:",len*br)
+
+def rectangle_peri(len,br):
+    print("perimeter",2*(len+br))
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import arithmetic as ar
+import geomatric as geo
 print("hello world")
 ar.add(23,34)
 ar.sub(34,23)
+geo.rectangle_area(10,5)
+geo.rectangle_peri(6,12)
